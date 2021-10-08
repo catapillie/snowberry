d0134dfda155f29ecefe161d12d4a9c6acb1d488
catapillie <espritox@gmail.com>
2021-10-08 19:07:12 +0200

    Create empty Everest code mod

---
+++ MapEditorMod/Module.cs
@@ -0,0 +1,21 @@
+﻿using Celeste.Mod;
+
+namespace MapEditorMod {
+    public class Module : EverestModule {
+        public static Module Instance {
+            get;
+            private set;
+        }
+
+        public Module() {
+            Instance = this;
+        }
+
+        public override void Load() { }
+
+        public override void Unload() { }
+
+        public static void Log(LogLevel level, string message)
+            => Logger.Log(level, "Map Maker Mod", message);
+    }
+}
