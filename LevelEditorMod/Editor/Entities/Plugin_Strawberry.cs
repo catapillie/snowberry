800adb49c76e2403081af90aa89dc8d3fe601f87
catapillie <espritox@gmail.com>
2021-10-09 19:25:57 +0200

    Add Strawberry entity plugin

---
+++ LevelEditorMod/Editor/Entities/Plugin_Strawberry.cs
@@ -0,0 +1,11 @@
+﻿using Celeste;
+
+namespace LevelEditorMod.Editor.Entities {
+    [EntityPlugin("strawberry")]
+    public class Plugin_Strawberry : EntityPlugin {
+        internal override void Render() {
+            base.Render();
+            GFX.Game["collectables/strawberry/normal00"].DrawCentered(Position);
+        }
+    }
+}
