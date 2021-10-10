5e299e642e47637b54d4289bb7060bc64c29e64d
catapillie <espritox@gmail.com>
2021-10-10 23:41:05 +0200

    Add basic spikes plugin

---
+++ LevelEditorMod/Editor/Entities/Plugin_Spikes.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace LevelEditorMod.Editor.Entities {
+    [EntityPlugin("spikesUp")]
+    [EntityPlugin("spikesDown")]
+    [EntityPlugin("spikesLeft")]
+    [EntityPlugin("spikesRight")]
+    public class Plugin_Spikes : EntityPlugin {
+        [EntityOption("type")] public string Type = "default";
+
+        internal override void Render() {
+            base.Render();
+            
+            switch (Name) {
+                case "spikesUp":
+                default:
+                    Draw.Rect(Position - Vector2.UnitY * 5, Width, 5, Color.White * 0.25f);
+                    break;
+
+                case "spikesDown":
+                    Draw.Rect(Position, Width, 5, Color.White * 0.25f);
+                    break;
+
+                case "spikesLeft":
+                    Draw.Rect(Position - Vector2.UnitX * 5, 5, Height, Color.White * 0.25f);
+                    break;
+
+                case "spikesRight":
+                    Draw.Rect(Position, 5, Height, Color.White * 0.25f);
+                    break;
+            }
+        }
+    }
+}
