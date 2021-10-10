1716b31e99639a3e3caafa0ee564ff537806fbda
catapillie <espritox@gmail.com>
2021-10-10 02:01:47 +0200

    Added refill entity plugin

---
+++ LevelEditorMod/Editor/Entities/Plugin_Refill.cs
@@ -0,0 +1,14 @@
+﻿using Celeste;
+
+namespace LevelEditorMod.Editor.Entities {
+    [EntityPlugin("refill")]
+    public class Plugin_Refill : EntityPlugin {
+        [EntityOption("twoDash")] public bool TwoDash;
+        [EntityOption("oneUse")] public bool OneUse;
+
+        internal override void Render() {
+            base.Render();
+            GFX.Game[$"objects/{(TwoDash ? "refillTwo" : "refill")}/idle00"].DrawOutlineCentered(Position);
+        }
+    }
+}
