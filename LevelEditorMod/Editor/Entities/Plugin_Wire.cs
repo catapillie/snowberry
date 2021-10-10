4fd9dc13211c0fb90014aa6febc1228cf69a9b92
catapillie <espritox@gmail.com>
2021-10-10 13:35:16 +0200

    Add wire plugin

---
+++ LevelEditorMod/Editor/Entities/Plugin_Wire.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace LevelEditorMod.Editor.Entities {
+    [EntityPlugin("wire")]
+    public class Plugin_Wire : EntityPlugin {
+        [EntityOption("above")] public bool Above;
+        [EntityOption("color")] public string Hex = "595866";
+
+        private Color Color => Calc.HexToColor(Hex);
+
+        internal override void Render() {
+            base.Render();
+
+            Vector2 start = Position;
+            Vector2 end = GetNodes()[0];
+            Vector2 control = (start + end) / 2f + Vector2.UnitY * 24f;
+
+            SimpleCurve curve = new SimpleCurve(start, end, control);
+            curve.Render(Color, 16);
+        }
+    }
+}
