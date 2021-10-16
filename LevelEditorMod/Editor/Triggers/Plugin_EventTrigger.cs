97f81b20e7b2bceff502e3e351a98e6395b236db
catapillie <espritox@gmail.com>
2021-10-16 15:18:35 +0200

    Added optionless triggers, format trigger name

--- LevelEditorMod/Editor/Triggers/Plugin_EventTrigger.cs
+++ LevelEditorMod/Editor/Triggers/Plugin_EventTrigger.cs
@@ -1,5 +1,6 @@
 ﻿namespace LevelEditorMod.Editor.Triggers {
     [Plugin("eventTrigger")]
+    [Plugin("creditsTrigger")]
     public class Plugin_EventTrigger : Plugin_Trigger {
         [Option("event")] public string Event = "";
     }
--- LevelEditorMod/Editor/Triggers/Plugin_Trigger.cs
+++ LevelEditorMod/Editor/Triggers/Plugin_Trigger.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.Xna.Framework;
 using Monocle;
+using System.Text.RegularExpressions;
 
 namespace LevelEditorMod.Editor.Triggers {
-    public abstract class Plugin_Trigger : Entity {
+    [Plugin("checkpointBlockerTrigger")]
+    [Plugin("goldenBerryCollectTrigger")]
+    [Plugin("lookoutBlocker")]
+    [Plugin("stopBoostTrigger")]
+    [Plugin("windAttackTrigger")]
+    [Plugin("birdPathTrigger")]
+    [Plugin("everest/completeAreaTrigger")]
+    public class Plugin_Trigger : Entity {
         protected virtual Color Color { get; } = Calc.HexToColor("0c5f7a");
+        protected string Text { get; private set; }
+
+        public override void Initialize() {
+            base.Initialize();
+            Text = string.Join(" ", Regex.Split(char.ToUpper(Name[0]) + Name.Substring(1), @"(?=[A-Z])"));
+        }
 
         public override void Render() {
             base.Render();
@@ -12,7 +26,7 @@ namespace LevelEditorMod.Editor.Triggers {
             Draw.Rect(rect, Color * 0.3f);
             Draw.HollowRect(rect, Color);
 
-            Fonts.Pico8.Draw(Name, new Vector2(rect.X + rect.Width / 2f, rect.Y + rect.Height / 2f), Vector2.One, Vector2.One * 0.5f, Color.Black); ;
+            Fonts.Pico8.Draw(Text, new Vector2(rect.X + rect.Width / 2f, rect.Y + rect.Height / 2f), Vector2.One, Vector2.One * 0.5f, Color.Black); ;
         }
     }
 }
