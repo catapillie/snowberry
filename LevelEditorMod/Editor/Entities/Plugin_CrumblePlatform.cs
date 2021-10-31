24b738ecc64dc9db0f1ad071bb791e63f9c61dd3
Luna <izzeldeen03@gmail.com>
2021-10-31 21:48:46 +0000

    crumble blocks

---
+++ LevelEditorMod/Editor/Entities/Plugin_CrumblePlatform.cs
@@ -0,0 +1,23 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace LevelEditorMod.Editor.Entities {
+    [Plugin("crumbleBlock")]
+    public class Plugin_CrumblePlatform : Entity {
+
+        public override void Render() {
+            base.Render();
+
+			// TODO: custom textures
+			MTexture mTexture2 = GFX.Game["objects/crumbleBlock/" + AreaData.Get(Room.Map.From).CrumbleBlock];
+                
+
+			for(int j = 0; (float)j < Width; j += 8) {
+				int num2 = (int)((Math.Abs(X) + (float)j) / 8f) % 4;
+				mTexture2.GetSubtexture(num2 * 8, 0, 8, 8).DrawCentered(new Vector2(4 + j + X, 4f + Y));
+			}
+		}
+    }
+}
