f8a4d8863c558d22c479b851b4630dc5001062fa
Luna <izzeldeen03@gmail.com>
2021-11-01 21:55:10 +0000

    toolbar & selection tool

---
+++ LevelEditorMod/Editor/Tool.cs
@@ -0,0 +1,102 @@
+﻿using LevelEditorMod.Editor.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using System;
+using System.Collections.Generic;
+
+namespace LevelEditorMod.Editor {
+
+	// Object Select (rect/lasso??), Object Placement (entity/trigger/decal), Tile Select, Tile Brush (fg/bg)
+	// selection filters (entity/trigger/decal, layers/tags??) can be handled in the panel
+	public abstract class Tool {
+
+		public static IList<Tool> Tools = new List<Tool>() { new SelectionTool(), new TileBrushTool() };
+
+		public abstract string GetName();
+
+		public abstract UIElement CreatePanel();
+
+		public abstract void Update();
+
+		public void Render() { }
+
+		public IList<string> GetModes() => new List<string>() { "default" };
+	}
+
+	public class SelectionTool : Tool {
+
+		static bool canSelect;
+
+		public override string GetName() {
+			return "Object Select";
+		}
+
+		public override UIElement CreatePanel() {
+			return new UISelectionPanel() {
+				Width = 160,
+			};
+		}
+
+		public override void Update() {
+			var editor = Editor.GetCurrent();
+
+			bool shift = MInput.Keyboard.CurrentState[Keys.LeftShift] == KeyState.Down || MInput.Keyboard.CurrentState[Keys.RightShift] == KeyState.Down;
+
+			if(MInput.Mouse.CheckLeftButton && shift) {
+				if(MInput.Mouse.PressedLeftButton) {
+					Point mouse = new Point((int)Editor.Mouse.World.X, (int)Editor.Mouse.World.Y);
+
+					canSelect = true;
+					if(Editor.SelectedEntities != null) {
+						foreach(EntitySelection s in Editor.SelectedEntities) {
+							if(s.Contains(mouse)) {
+								canSelect = false;
+								break;
+							}
+						}
+					}
+				}
+
+				if(canSelect && Editor.SelectedRoom != null) {
+					int ax = (int)Math.Min(Editor.Mouse.World.X, editor.worldClick.X);
+					int ay = (int)Math.Min(Editor.Mouse.World.Y, editor.worldClick.Y);
+					int bx = (int)Math.Max(Editor.Mouse.World.X, editor.worldClick.X);
+					int by = (int)Math.Max(Editor.Mouse.World.Y, editor.worldClick.Y);
+					Editor.Selection = new Rectangle(ax, ay, bx - ax, by - ay);
+
+					Editor.SelectedEntities = Editor.SelectedRoom.GetSelectedEntities(Editor.Selection.Value);
+				} else if(Editor.SelectedEntities != null) {
+					Vector2 worldSnapped = (Editor.Mouse.World / 8).Floor() * 8;
+					Vector2 worldLastSnapped = (Editor.Mouse.WorldLast / 8).Floor() * 8;
+					Vector2 move = worldSnapped - worldLastSnapped;
+					foreach(EntitySelection s in Editor.SelectedEntities)
+						s.Move(move);
+				}
+			} else
+				Editor.Selection = null;
+
+			if(MInput.Mouse.ReleasedLeftButton && shift) {
+				if(canSelect && editor.ToolPanel is UISelectionPanel selectionPanel)
+					selectionPanel.Display(Editor.SelectedEntities);
+			}
+		}
+	}
+
+	public class TileBrushTool : Tool {
+
+		public override string GetName() {
+			return "Tile Brush";
+		}
+
+		public override UIElement CreatePanel() {
+			return new UIElement() {
+				Width = 120
+			};
+		}
+
+		public override void Update() {
+
+		}
+	}
+}
--- LevelEditorMod/Editor/UI/UIElement.cs
+++ LevelEditorMod/Editor/UI/UIElement.cs
@@ -4,15 +4,24 @@ using System.Collections.Generic;
 namespace LevelEditorMod.Editor.UI {
     public class UIElement {
         private readonly List<UIElement> children = new List<UIElement>();
+        private readonly List<UIElement> toRemove = new List<UIElement>();
+        private readonly List<UIElement> toAdd = new List<UIElement>();
+        private bool canModify = true;
         protected UIElement Parent;
 
         public Vector2 Position;
         public int Width, Height;
 
         public virtual void Update(Vector2 position = default) {
+            canModify = false;
             foreach (UIElement element in children)
                 element.Update(position + element.Position);
+            canModify = true;
             children.RemoveAll(e => e == null);
+            toRemove.ForEach(a => children.Remove(a));
+            toRemove.Clear();
+            toAdd.ForEach(a => Add(a));
+            toAdd.Clear();
         }
 
         public virtual void Render(Vector2 position = default) {
@@ -31,11 +40,14 @@ namespace LevelEditorMod.Editor.UI {
         }
 
         public void Add(UIElement element) {
-            if (element.Parent == null) {
-                children.Add(element);
-                element.Parent = this;
-                element.Initialize();
-            }
+            if(canModify) {
+                if(element.Parent == null) {
+                    children.Add(element);
+                    element.Parent = this;
+                    element.Initialize();
+                }
+            } else
+                toAdd.Add(element);
         }
 
         public void AddBelow(UIElement element) {
@@ -44,10 +56,25 @@ namespace LevelEditorMod.Editor.UI {
                 element.Position += new Vector2(0, child.Height);
         }
 
+        public void AddRight(UIElement element) {
+            Add(element);
+            foreach(UIElement child in children)
+                element.Position += new Vector2(child.Width, 0);
+        }
+
         public void Clear() {
             foreach (UIElement element in children)
                 element?.Destroy();
             children.Clear();
+        }
+
+        public void Remove(UIElement elem) {
+            toRemove.Add(elem);
+        }
+
+        public void RemoveAll(ICollection<UIElement> elems) {
+			foreach(var item in elems) 
+                Remove(item);
         }
     }
 }
