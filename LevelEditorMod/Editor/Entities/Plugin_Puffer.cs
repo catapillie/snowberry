22cf39f4947e6a32cec936f236ab02a98effbaff
Luna <izzeldeen03@gmail.com>
2021-11-15 18:40:23 +0000

    pufferfish

---
+++ LevelEditorMod/Editor/Entities/Plugin_Puffer.cs
@@ -0,0 +1,19 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace LevelEditorMod.Editor.Entities {
+    [Plugin("eyebomb")]
+    public class Plugin_Puffer : Entity {
+        [Option("right")] public bool Right;
+
+        public override void Render() {
+            base.Render();
+
+            GFX.Game[$"objects/puffer/idle00"].DrawOutlineCentered(Position, Color.White, new Vector2(Right ? 1 : -1, 1));
+        }
+
+        public static void AddPlacements() {
+            Placements.Create("Puffer", "eyebomb");
+        }
+    }
+}
