d85bc6fff3b4086ce1e6e1808ae231696eb0f7bc
Luna <izzeldeen03@gmail.com>
2021-11-16 15:59:41 +0000

    torches

---
+++ LevelEditorMod/Editor/Entities/Plugin_Torch.cs
@@ -0,0 +1,20 @@
+﻿using Celeste;
+using System.Collections.Generic;
+
+namespace LevelEditorMod.Editor.Entities {
+    [Plugin("torch")]
+    public class Plugin_Torch : Entity {
+        [Option("startLit")] public bool Lit;
+
+        public override void Render() {
+            base.Render();
+            
+            GFX.Game[$"objects/temple/{(Lit ? "litTorch" : "torch")}03"].DrawCentered(Position);
+        }
+
+        public static void AddPlacements() {
+            Placements.Create("Torch", "torch", new Dictionary<string, object>() { { "red", false } });
+            Placements.Create("Torch (Lit)", "torch", new Dictionary<string, object>() { { "startLit", true } });
+        }
+    }
+}
