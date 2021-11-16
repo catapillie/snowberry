560957dfd7afcb14b0e70eb5038db1a8a7831529
Luna <izzeldeen03@gmail.com>
2021-11-16 11:13:06 +0000

    jellyfish

---
+++ LevelEditorMod/Editor/Entities/Plugin_Glider.cs
@@ -0,0 +1,38 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace LevelEditorMod.Editor.Entities {
+    [Plugin("glider")]
+    public class Plugin_Glider : Entity {
+
+        [Option("bubble")] public bool Bubble = false;
+        [Option("tutorial")] public bool Tutorial = false;
+
+        public override void Render() {
+            base.Render();
+            GFX.Game["objects/glider/idle0"].DrawOutlineCentered(Position);
+            if(Bubble) {
+                for(int i = 0; i < 24; i++) {
+                    Draw.Point(Position + PlatformAdd(i), PlatformColor(i));
+                }
+            }
+        }
+
+        private Vector2 PlatformAdd(int num) {
+            return new Vector2(-12 + num, -5 + 1.8f);
+        }
+
+        private Color PlatformColor(int num) {
+            if(num <= 1 || num >= 22) {
+                return Color.White * 0.4f;
+            }
+
+            return Color.White * 0.8f;
+        }
+
+        public static void AddPlacements() {
+            Placements.Create("Jellyfish", "glider");
+        }
+    }
+}
